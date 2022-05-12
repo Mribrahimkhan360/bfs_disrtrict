824eb27d3f89b7c275ec4ff063137d1008e4036d
Ibrahim Khan <73376843+Mribrahimkhan360@users.noreply.github.com>
2022-05-12 12:45:19 +0600

    Add files via upload

---
+++ ten-ten.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	/* code */
+	int n;
+	cin>>n;
+	int a[n][n];
+	// int x,y;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; ++i)
+		{
+			/* code */
+		}
+		/* code */
+	}
+
+	return 0;
+}
+
